155ea886fb3dbd5a3418abe72e94cfc94503b1b7
Maksym Stelmakh <stelmakh1994@gmail.com>
2022-10-11 16:26:19 +0300

    mobile-menu.js  error fix

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,6 +1,5 @@
 (() => {
     const menuBtnRef = document.querySelector("[data-menu-button]");
-    const mobileMenuRef = document.querySelector("[data-menu]");
 
     menuBtnRef.addEventListener("click", () => {
         const expanded =
@@ -9,6 +8,5 @@
         menuBtnRef.classList.toggle("is-open");
         menuBtnRef.setAttribute("aria-expanded", !expanded);
 
-        mobileMenuRef.classList.toggle("is-open");
     });
 })();
