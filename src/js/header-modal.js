d10b7e7b0795228755f4d3f21fc8284158860c1a
Maksym Stelmakh <stelmakh1994@gmail.com>
2022-10-13 10:39:56 +0300

    modal-header js data attr change

--- src/js/header-modal.js
+++ src/js/header-modal.js
@@ -1,14 +1,14 @@
 (() => {
     const refs = {
-        openModalBtnTablet: document.querySelector('[data-modal-tablet-open]'),
-        openModalBtn: document.querySelector('[data-modal-mobile-open]'),
+        openModalBtn1: document.querySelector('[data-header-modal-open1]'),
+        openModalBtn2: document.querySelector('[data-header-modal-open-2]'),
         closeModalBtn: document.querySelector('[data-header-modal-close]'),
         modal: document.querySelector('[data-header-modal]'),
         body: document.body,
     };
 
-    refs.openModalBtnTablet.addEventListener('click', toggleModal);
-    refs.openModalBtn.addEventListener('click', toggleModal);
+    refs.openModalBtn1.addEventListener('click', toggleModal);
+    refs.openModalBtn2.addEventListener('click', toggleModal);
     refs.closeModalBtn.addEventListener('click', toggleModal);
 
     function toggleModal() {
